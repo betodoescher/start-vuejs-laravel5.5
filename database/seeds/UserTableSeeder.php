b16c2c700fa1fabc046de2688a150995e2829d32
jaysongyn <jayson.inf@gmail.com>
2018-01-31 09:07:27 -0200

    criei usuario admin@admin.com e correção do faker cpf

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -12,5 +12,11 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         factory(\App\User::class,5)->create();
+        factory(\App\User::class,1)->create([ 'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' =>  bcrypt('secret'),
+            'tipo_usuario' => 1,
+            'cpf' => '02658030458',
+            'remember_token' => str_random(10),]);
     }
 }
